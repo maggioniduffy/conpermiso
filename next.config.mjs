361607d81969f0641dce1b56958d4faaab4dbc22
maggioniduffy <fausmaggioni5@gmail.com>
2026-04-20 14:44:11 -0300

    centralized kkapp in header && new host for images

--- next.config.mjs
+++ next.config.mjs
@@ -74,6 +74,11 @@ const nextConfig = {
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "conpermiso-images.s3.amazonaws.com" },
+      {
+        protocol: "https",
+        hostname: "conpermiso-images.s3.us-east-2.amazonaws.com",
+      },
       {
         protocol: "https",
         hostname: "conpermiso-images-prod.s3.amazonaws.com",
